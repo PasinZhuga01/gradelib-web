6fdcc4358c6e644cee1ac8fac4339a636df48bd6
PasinZhuga01 <sergijumanskij@gmail.com>
2026-05-21 01:31:42 +0300

    chore: GRA-15 initialize project, configure architecture
    - initialize project
    - configure environment configuration

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+import { environment } from '../environments/environment';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected title = environment.apiUrl;
+}
